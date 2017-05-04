54baf83d72eb0ee0280c607d2b9842993566fc8b
Vincent Yu <vincentyu@CEN-0033681.local>
2017-05-04 11:20:53 -0400

    merge with updated supermarket script

--- scripts/foodStores.py
+++ scripts/foodStores.py
@@ -147,34 +147,64 @@ def flushing():
     coordsFlushing = []
 
     #Generate all the links for Flushing restaurants
-    numRestaurants = 92
+    numRestaurants = 60
     links = []
 
     for i in list(range(0,numRestaurants,10)):
         link = "https://www.yelp.com/search?find_desc=supermarkets&find_loc=11354,11355,+New+York,+NY&start=" + str(i)
         links.append(link)
 
-    #Scrape for all addresses
+    #Scrape for all addresses and names of supermarkets
     addresses = []
+    names = []
+
+    #All supermarkets have a name
+    #Some supermarkets are located within other supermarkets (i.e. within New World Mall)
+    #In these specific cases, there is no given address for the supermarket
+    #In order to keep a 1-to-1 correspondence of names and addresses so we have a proper map, we discard these no address supermarkets
+    #They are still mapped however, since the host supermarket is still mapped (i.e. New World Mall is still mapped)
+    haveNameLFaddress = False
 
     for i in links:
         html = requests.get(i).text
         lines = html.split("\n")
 
+        #Used when searching for names
+        beginIndex = len("><span >")
+        endIndex = len("</span></a>")
+
         for index,line in enumerate(lines):
-            match = re.search(r"<address>", line)
-            if match == None:
-                pass
-            else:
+
+            matchName = re.search(r"><span >(.*)/span></a>", line)
+            if matchName != None:
+                name = str(matchName.group())
+                #some name (below) is on the html of every single page, we ignore this
+                if name == "><span >NY Convenience Store &amp; Deli</span></a>":
+                    pass
+                else:
+                    if haveNameLFaddress == False:
+                        #Take out the front and end tags we searched for
+                        name = name[beginIndex:len(name)-endIndex]
+                        #Convert remaining html elements to English
+                        name = name.replace('<span class="highlighted">', "")
+                        name = name.replace('</span>', "")
+                        name = name.replace('&amp;', "&")
+                        names.append(name)
+                        haveNameLFaddress = True 
+                        print(name + " -> ", end="")
+
+            matchAddress = re.search(r"<address>", line)
+            if matchAddress != None:
                 #address is always on the next line
                 address = lines[index+1]
-                #some address on the html that is on every single page and it obviously does not belong
+                #some address (below) is on the html of every single page and it obviously does not belong (Jamaica)
                 if address == "        14692 Guy R Brewer Blvd<br>Jamaica, NY 11434":
                     pass
                 else:
                     address = address.replace("        ","")
                     address = address.replace("<br>", ", ")
                     print(address)
+                    haveNameLFaddress = False #found address for previous name of supermarket
                     addresses.append(address)
 
     #Hold NoneTypes
@@ -189,7 +219,7 @@ def flushing():
                     print("Valid count: %d, address: %s"%(len(coordsFlushing),i))
                     coordsFlushing.append([location.latitude, location.longitude])
                     storesFlushingMap.add_child(folium.Marker(location=[location.latitude, location.longitude],
-                                            popup =(folium.Popup("WHEREISVINCENT"))))
+                                            popup =(folium.Popup(names[addresses.index(i)]))))
                     time.sleep(1)
                 #Improper formatting of addresses cannot be mapped (thanks Yelp)
                 else:
