15588bac53c5b2d685a8eb87f82ff4f529372635
Jane Chen <jane.chen@macaulay.cuny.edu>
2017-05-04 09:18:55 -0400

    fixed coordinates

--- scripts/libraries.py
+++ scripts/libraries.py
@@ -8,20 +8,32 @@ csv file was from https://nycopendata.socrata.com/Recreation/Queens-Library-Bran
 
 import pandas as pd
 import folium
+from pygeocoder import Geocoder
 
 def isClosed(row):
     return row['Mn'] == row['Tu'] == row['We'] == \
     row['Th'] == row['Fr'] == row['Sa'] == row['Su'] == 'closed'
+
+def fixAddress(address):
+    tmp = address.split(' ')
+    if (tmp[0].isnumeric() and tmp[1].isnumeric()):
+        tmp[0] = tmp[0] + '-' + tmp[1]
+    del tmp[1]
+    return ' '.join(tmp)
 
 libraries = pd.read_csv('../data/Queens_Library_Branches.csv')
 librariesMap = folium.Map(location=[40.7599029,-73.843553], zoom_start=15)
 
 for index, row in libraries.iterrows():
     loc = row['Location 1'].split('\n')
-    coords = loc[2].split(', ')
-    lat = float(coords[0][1:])
-    lon = float(coords[1][:-1])
     name = row['name']
+
+    results = Geocoder.geocode(loc[0] + " " + loc[1])
+    # print(type(results[0].coordinates))
+    coords = results[0].coordinates
+    lat = coords[0]
+    lon = coords[1]
+
     closed = isClosed(row)
 
     if closed:
