88d648e269c13543a6a849e5fe0729c80ecc9e00
Vincent Yu <yuv@bxscience.edu>
2017-05-03 22:50:18 -0400

    maps supermarket locations near Flushing

---
+++ scripts/supermarket_locations.py
@@ -0,0 +1,72 @@
+# ------------------------------
+#    supermarket_locations.py
+#
+#    Gets the location of all supermarkets in the Flushing directory of Yelp.
+#    Plots these locations on a map.
+# ------------------------------
+
+import requests
+import re
+import folium
+from geopy.geocoders import Nominatim
+from folium.plugins import MarkerCluster
+import time
+from geopy.exc import GeocoderTimedOut
+
+#Generate all the links
+numRestaurants = 475
+links = []
+
+for i in list(range(0,numRestaurants,10)):
+	link = "https://www.yelp.com/search?find_desc=supermarkets&find_loc=Flushing,+Queens,+NY&start=" + str(i)
+	links.append(link)
+
+#Scrape for all addresses
+addresses = []
+
+for i in links:
+	html = requests.get(i).text
+	lines = html.split("\n")
+
+	for index,line in enumerate(lines):
+		match = re.search(r"<address>", line)
+		if match == None:
+			pass
+		else:
+			#address is always on the next line
+			address = lines[index+1]
+			#some address on the html that is on every single page and it obviously does not belong
+			if address == "        14692 Guy R Brewer Blvd<br>Jamaica, NY 11434":
+				pass
+			else:
+				address = address.replace("        ","")
+				address = address.replace("<br>", ", ")
+				print(address)
+				addresses.append(address)
+
+#Map all addresses
+coords = []
+NoneType = []
+geolocator = Nominatim()
+
+for i in addresses:
+	try:
+		location = geolocator.geocode(i)
+		if type(location) != type(None):
+			coords.append([location.longitude, location.latitude])
+			print("Valid count: %d, address: %s"%(len(coords),i))
+			time.sleep(1)
+		#Improper formatting of addresses cannot be mapped (thanks Yelp)
+		else:
+			NoneType.append(i)
+			print("Invalid count: %d, address: %s"%(len(NoneType),i))
+			time.sleep(1)
+	except GeocoderTimedOut as e:
+		print("Timed out with address %s"%i)
+
+mapRestaurants = folium.Map([40.77, -73.83], zoom_start=13)
+mapRestaurants.add_children(MarkerCluster(locations=coords))
+mapRestaurants.save(outfile="supermarket_locations.html")
+
+
+
